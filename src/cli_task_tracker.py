33009d4b6fcf976b595ccc7f9e46e2728ede6c5e
fhitzke <florentin.hitzke@gmail.com>
2026-05-31 14:37:22 +0200

    Added persistence logic to all functionality. Fixed reading/writing from json to database field. Simplified get and check task.

--- src/cli_task_tracker.py
+++ src/cli_task_tracker.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field, asdict
 from src.task import Task
-from typing import List
+from typing import List, Tuple
 from pathlib import Path
 import datetime
 import json
@@ -17,24 +17,47 @@ class TaskTracker:
             self.database = {}
             with open(self.directory, "w") as write_file:
                 json.dump(self.database, write_file)
+            print(f"Task registry created.")
         else:
             with open(self.directory, "r") as read_file:
                 self.database = json.load(read_file)
-            self.unique_id = self.database["id"]
-            self.tasks = [Task(**t) for t in self.database]
+            self.unique_id = int(list(self.database.keys())[-1])
+            self.tasks = [
+                Task(**{
+                    **t,
+                    "createdAt": datetime.datetime.strptime(t["createdAt"], "%a %d %b %Y, %H:%M"),
+                    "updatedAt": datetime.datetime.strptime(t["updatedAt"], "%a %d %b %Y, %H:%M"),
+                }) for t in self.database.values()
+            ]
+            print(f"Task registry loaded.\n")
 
     @staticmethod
     def _format_datetime(date: datetime.datetime) -> str:
         return date.strftime("%a %d %b %Y, %H:%M")
+    
+    def save(self) -> None:
+        try:
+            with open(self.directory, "w") as write_file:
+                json.dump(self.database, write_file)
+        except OSError as e: 
+            raise OSError(f"Failed to write file: {e}")
+        except TypeError as e:
+            raise TypeError(f"Database contains non-serializable data: {e}")
+    
+    def get_task(self, id: int) -> Tuple[int, Task]:
+        ids: List[int] = [task.id for task in self.tasks]
+        if id not in ids:
+            raise ValueError(f"Task with the id: {id} is not in the task list.")
+        for task_idx, task in enumerate(self.tasks):
+            if task.id == id:
+                return (task_idx, task)
 
     def add(self, description: str) -> str:
         if description.strip() == "":
             raise ValueError(f"Description must not be empty.")
-        
         task_id: int = self.unique_id
         task = Task(id=task_id, description=description)
         self.tasks.append(task)
-
         # build/update the database dict 
         self.database[task_id] = asdict(task)
         self.unique_id += 1
@@ -42,45 +65,35 @@ class TaskTracker:
         return f"Task added successfully (ID: {task_id})"
 
     def update(self, id: int, description: str) -> None:
-        ids: List[int] = [task.id for task in self.tasks]
         if description.strip() == "":
             raise ValueError(f"Description must not be empty.")
-        if id not in ids:
-            raise ValueError(f"Task with the ID: {id} is not in the task list.")
-        for task in self.tasks:
-            if task.id == id:
-                task.description = description
-                task.updatedAt = self._format_datetime(datetime.datetime.now())
+        task = self.get_task(id)[1]
+        task.description = description
+        task.updatedAt = self._format_datetime(datetime.datetime.now())
+        self.database[str(task.id)] = asdict(task)
+        self.save()
 
     def delete(self, id: int) -> None:
-        ids: List[int] = [task.id for task in self.tasks]
-        if id not in ids:
-            raise ValueError(f"Task with the ID: {id} is not in the task list.")
-        for idx, task in enumerate(self.tasks):
-            if task.id == id:
-                self.tasks.pop(idx)
+        task = self.get_task(id)
+        self.tasks.pop(task[0])
+        del self.database[str(task[1].id)]
+        self.save()
 
     def change_status(self, id: int, status: str) -> None:
-        ids: List[int] = [task.id for task in self.tasks]
-        if id not in ids:
-            raise ValueError(f"Task with the ID: {id} is not in the task list.")
         if status not in ["in-progress", "done"]:
             raise ValueError(f"Status must be either of (\"in-progress\", \"done\").")
-        for task in self.tasks:
-            if task.id == id:
-                task.status = status
-                task.updatedAt = self._format_datetime(datetime.datetime.now())
-    
+        task = self.get_task(id)[1]
+        task.status = status
+        task.updatedAt = self._format_datetime(datetime.datetime.now())
+        self.database[str(task.id)] = asdict(task)
+        self.save()
+
     def list(self, status: str = "all") -> str:
         if status in ["done", "todo", "in-progress"]:
             tasks = [task for task in self.tasks if task.status == status]
         else:
             tasks = self.tasks
         return "\n".join(repr(task) for task in tasks)
-    
-    def save(self) -> None:
-        with open(self.directory, "w") as write_file:
-            json.dump(self.database, write_file)
     
 
 
--- src/core.py
+++ src/core.py
@@ -3,19 +3,16 @@ from src.cli_task_tracker import TaskTracker
 
 def main():
     task_tracker = TaskTracker()
-    print(task_tracker.add("Buy groceries"))
-    print(task_tracker.add("Walk the dog"))
 
+    # print(task_tracker.add("Buy groceries"))
+    # print(task_tracker.add("Walk the dog"))
     # print(task_tracker.list())
 
     # task_tracker.update(0, "Buy groceries and cook dinner")
-
-    # print(task_tracker.list("todo"))
-
-    # task_tracker.delete(0)
-
     # print(task_tracker.list())
 
-    # task_tracker.change_status(1, "done")
+    # task_tracker.delete(0)
+    # print(task_tracker.list())
 
-    # print(task_tracker.list("done"))
+    task_tracker.change_status(1, "done")
+    print(task_tracker.list("done"))
